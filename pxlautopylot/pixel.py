4ee170f1c3ed8dfc3d30365cb550022373a876fc
Eloriq <6159949+Eloriq@users.noreply.github.com>
2022-09-07 17:53:51 +0200

    release device context when done

--- pxlautopylot/pixel.py
+++ pxlautopylot/pixel.py
@@ -12,6 +12,7 @@ class Pixel:
         desktop_window_id = win32gui.GetDesktopWindow()
         desktop_window_dc = win32gui.GetWindowDC(desktop_window_id)
         long_color = win32gui.GetPixel(desktop_window_dc, self.x, self.y)
+        win32gui.ReleaseDC(desktop_window_id, desktop_window_dc)
         int_color = int(long_color)
         return Color((int_color & 0xff), ((int_color >> 8) & 0xff), ((int_color >> 16) & 0xff))
 
